69de428642653109683ba0f52783d66bc5184030
Anisse-Imer <anissezou@gmail.com>
2025-05-20 15:22:12 +0200

    Localisation - object

--- app/src/main/java/fr/upjv/geotrack/models/Localisation.java
+++ app/src/main/java/fr/upjv/geotrack/models/Localisation.java
@@ -1,5 +1,32 @@
 package fr.upjv.geotrack.models;
 
-public class Localisation {
+import com.google.firebase.firestore.auth.User;
 
+import java.util.Date;
+import java.util.HashMap;
+
+public class Localisation {
+    private String id;
+    private String userUUID;
+    private Date timestamp;
+    private float longitude;
+    private float latitude;
+
+    public Localisation(String Id, String UserUUID, Date Time, float Longitude, float Latitude) {
+        this.id = Id;
+        this.userUUID = UserUUID;
+        this.timestamp = Time;
+        this.longitude = Longitude;
+        this.latitude = Latitude;
+    }
+
+    public HashMap<String, Object> toJson() {
+        HashMap<String, Object> hash = new HashMap<>();
+        hash.put("id", this.id);
+        hash.put("userUUID", this.userUUID);
+        hash.put("timestamp", this.timestamp);
+        hash.put("longitude", this.longitude);
+        hash.put("latitude", this.latitude);
+        return hash;
+    }
 }
